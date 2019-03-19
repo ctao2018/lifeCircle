adb49d734479881aacb25878203aae7d5eb5ce27
ctao666 <819473894@qq.com>
2019-03-19 18:52:40 +0800

    cc

--- pages/answer/answer.js
+++ pages/answer/answer.js
@@ -1,5 +1,6 @@
 const app = getApp();
-import {queryAllValidMaterials,uploadPic} from '../../config/api'
+import {queryAllValidMaterials} from '../../config/api'
+import env from '../../config/env'
 
 Page({
   data: {
@@ -10,7 +11,9 @@ Page({
     typeBack:'',
     changebtncol:false,
     questionId:'',
-    imgUp:''
+    imgUp:'',
+    textarea:'',
+    imgArr:[],
   },
 
   onLoad(options) {
@@ -74,25 +77,51 @@ Page({
   },
   //点击上传图片
   upLoadimg() {
-    my.chooseImage({
-      success: (res) => {
-        let imgSrc = res.apFilePaths[0];
-        console.log(imgSrc)
-        //this._uploadPic(imgSrc)
-      },
-    });
-  },
-  //上传图片
-  async _uploadPic(img) {
-    let result = await uploadPic({
-      file:img
-    },{'Content-Type': 'multipart/form-data'})
-    console.log('图片',result)
+    const token = my.getStorageSync({ key: "token" });
+    if(this.data.imgArr.length<8){
+      my.chooseImage({
+        success: (res) => {
+          let imgSrc = res.apFilePaths[0];
+          //console.log(imgSrc)
+          my.uploadFile({
+            header: {'Authorization': token.data},
+            url: env.base_url_dev + '/sharecom/oss/picture/upload', // 开发者服务器地址
+            filePath: imgSrc, // 要上传文件资源的本地定位符
+            fileName: 'file', // 文件名，即对应的 key, 开发者在服务器端通过这个 key 可以获取到文件二进制内容
+            fileType: 'image', // 文件类型，image / video / audio
+            success: (res) => {
+              let list = JSON.parse(res.data)
+              //console.log(list)
+              this.data.imgArr = this.data.imgArr.concat(list)
+              this.setData({imgArr:this.data.imgArr})
+              console.log(this.data.imgArr)
+            },
+            fail: (res) => {
+              my.alert({
+                title: '上传失败' 
+              });
+            },
+          });
+          //this._uploadPic(imgSrc)
+        },
+      });
+    } else{
+      my.showToast({
+        content: '上传的图片张数已超过最大值！'
+      });
+    }
     
   },
+  //点击删除照片
+  delPicFn(e) {
+    let index=e.currentTarget.dataset['index'];
+    this.data.imgArr.splice(index,1)
+    this.setData({imgArr:this.data.imgArr})
+  },
   //点击提交按钮
-  submitFn() {
+  submitFn(e) {
     this.setData({changebtncol:true,})
+    this.setData({textarea:e.detail.value.textarea})
     my.alert({
       title: '提交成功',
       content: '将在3-6个工作日内审核，审核通过后发放积分',

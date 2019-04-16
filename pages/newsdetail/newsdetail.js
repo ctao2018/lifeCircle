c49924c06b0cb1a2a8937d24e70fd1bf3d7d09e4
ctao666 <819473894@qq.com>
2019-04-16 18:04:50 +0800

    cc

---
+++ pages/newsdetail/newsdetail.js
@@ -0,0 +1,49 @@
+const app = getApp();
+import {queryFNewsInfoDeatailById} from '../../config/api'
+import parse from 'mini-html-parser2';
+Page({
+  data: {
+   id:'',
+   detail:[],
+   nodes:[],
+  },
+
+  onLoad(options) {
+    if(options){
+      this.setData({
+        id:options.id,
+      })
+    }
+  },
+  onShow() {
+    this.setData({
+      detail:[],
+    });
+    this._queryFNewsInfoDeatailById()
+  },
+  onReady() {
+    
+  },
+  
+  //详情
+  async _queryFNewsInfoDeatailById() {
+    let result = await queryFNewsInfoDeatailById(this.data.id)
+    console.log('详情',result)
+      
+      this.setData({
+        detail:result.data.data
+      })
+      this.changeNode()
+  },
+  changeNode() {
+    let html = this.data.detail.content
+    parse(html, (err, nodes) => {
+      if (!err) {
+        this.setData({
+          nodes,
+        });
+      }
+    })
+    console.log(this.data.nodes)
+  },
+});

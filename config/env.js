04afbebd7433bc7539a3cbe4ac825044c02fe80a
ctao666 <819473894@qq.com>
2019-07-11 10:49:46 +0800

    cc

--- config/env.js
+++ config/env.js
@@ -1,11 +1,11 @@
 const env = "dev";
 let base_url_dev,jump_url,pic_url;
-// base_url_dev = "https://test.szyibei.com/api";
-// jump_url = "https://test.szyibei.com/sbjccx/jumpPage/";
-// pic_url = "https://epay-test-ssr.oss-cn-shenzhen.aliyuncs.com/lifeCircle/";
-base_url_dev = "https://www.szyibei.com/api";
-jump_url = "https://www.szyibei.com/sbjccx/jumpPage/";
-pic_url = "https://epay-prod-ssr.oss-cn-shenzhen.aliyuncs.com/lifeCircle/";
+base_url_dev = "https://test.szyibei.com/api";
+jump_url = "https://test.szyibei.com/sbjccx/jumpPage/";
+pic_url = "https://epay-test-ssr.oss-cn-shenzhen.aliyuncs.com/lifeCircle/";
+// base_url_dev = "https://www.szyibei.com/api";
+// jump_url = "https://www.szyibei.com/sbjccx/jumpPage/";
+// pic_url = "https://epay-prod-ssr.oss-cn-shenzhen.aliyuncs.com/lifeCircle/";
 export default {
     base_url_dev,
     jump_url,
